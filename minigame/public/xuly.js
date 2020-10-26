d1e99bc81f9ef7f05bb3f575e9fe91780f2b2948
LA - TA MINH KHA <tmk001-dn-la@fhmc.com>
2020-10-26 11:07:36 +0700

    finished heroku

--- minigame/public/xuly.js
+++ minigame/public/xuly.js
@@ -1,5 +1,24 @@
 
-var  socket=io("http://localhost:3000"); 
+var  socket=io("https://hmdntool.herokuapp.com/"); 
+socket.on("server-goi-ds-hocvien",function(data){
+    $("#dshocvien").html("");
+    data.map(function(hocvien,index){
+       $("#dshocvien").append(`
+       <div class='hocvien'>
+                         <div class='hang1'>id:`+index+` || <span>`+hocvien.HOTEN+`</span></div>
+                         <div class='hang2'>`+hocvien.EMAIL+`-ĐT-`+hocvien.PHONE+`</div>
+       </div>
+       `);
+    })
+})
 $(document).ready(function(){
-    alert("hi")
+    $("#btnSend").click(function(){
+            socket.emit("client-goi-hocvien",
+            {
+                hoten:$("#txtHoten").val(),
+                email:$("#txtEmail").val(),
+                phone:$("#txtPhone").val()
+            }
+        );
+    });
 })
--- minigame/server.js
+++ minigame/server.js
@@ -4,11 +4,23 @@ app.use(express.static("public"));
 app.set("view engine","ejs");
 app.set("views","./views");
 var server=require("http").Server(app);
-server.listen(3000);
+server.listen(process.env.PORT||3000);
 var io=require("socket.io")(server);
+var mangHocVien=[];// mảng chứa tất cả học viên
 io.on("connection",function(socket){
     console.log("có người kết nối"+ socket.id);
+        socket.on("client-goi-hocvien",function(data){
+        mangHocVien.push(
+            new HocVien(data.hoten,data.email,data.phone)
+        );
+        io.sockets.emit("server-goi-ds-hocvien",mangHocVien);
+    })
 })
 app.get("/",function(rep,res){
     res.render("trangchu");
 });
+function HocVien(hoten,email,phone){
+    this.HOTEN=hoten;
+    this.EMAIL=email;
+    this.PHONE=phone;
+}
